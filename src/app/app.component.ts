a431b749c1b184bbb2dca9a9f5dcec6afe11d38c
ntweb <dm@artisanlab.it>
2022-04-02 19:57:27 +0200

    loggedIn state observer

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,27 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { Store } from '@ngrx/store';
+import { AppState } from './reducers';
+import { map, Observable } from 'rxjs';
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+    selector: 'app-root',
+    templateUrl: './app.component.html',
+    styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'angular-module';
+export class AppComponent implements OnInit{
+
+    title = 'angular-module';
+
+    isLoggedIn$: Observable<boolean> | undefined;
+    isLoggedOut$: Observable<boolean> | undefined;
+
+    constructor(private router: Router, private store: Store<AppState>) {
+
+    }
+
+    ngOnInit(): void {
+    }
+
+
 }
